95bb376d9d99dcb9ba2c4767f3c5cac2718a57d9
Taco <471948717@qq.com>
2017-09-01 17:24:50 +0800

    测试第一个版本

---
+++ src/components/commom/BottomNavigation.vue
@@ -0,0 +1,39 @@
+<template>
+<mu-paper class="bottom_nav">
+  <mu-bottom-nav :value="bottomNav" @change="handleChange">
+    <mu-bottom-nav-item value="首页" title="首页" icon="home"/>
+    <mu-bottom-nav-item value="发布" title="发布" icon="subject"/>
+    <mu-bottom-nav-item value="消息" title="消息" icon="message"/>
+    <mu-bottom-nav-item value="我的" title="我的" icon="person"/>
+  </mu-bottom-nav>
+</mu-paper>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      bottomNav: 'recents'
+    }
+  },
+  methods: {
+    handleChange (val) {
+      this.bottomNav = val
+    }
+  }
+}
+</script>
+<style media="screen">
+.bottom_nav .mu-bottom-nav-shift-wrapper {
+  display: flex;
+  justify-content: space-around;
+}
+
+.bottom_nav {
+  height: 3.5rem;
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  right: 0;
+}
+</style>
---
+++ src/components/home.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="wrapper">
+    <isheader></isheader>
+    <tabs class="tab"></tabs>
+    <BottomNavigation></BottomNavigation>
+  </div>
+</template>
+
+<script>
+import isheader from './commom/header.vue'
+import BottomNavigation from './commom/BottomNavigation.vue'
+import tabs from './commom/tabs.vue'
+import axios from 'axios'
+export default {
+  components:{
+    isheader,
+    tabs,
+    BottomNavigation
+  },
+  data(){
+    return {
+      msg:'aaaa'
+    }
+  }
+}
+</script>
+
+<style lang="css">
+.wrapper {
+    display: flex;
+    flex-direction: column;
+    height: 100vh;
+}
+
+.tab {
+    flex: 1;
+    margin: 9rem 0 5rem 0;
+}
+
+.header {
+    position: fixed;
+    top: 0;
+    left: 0;
+    right: 0;
+    z-index: 1;
+}
+</style>
